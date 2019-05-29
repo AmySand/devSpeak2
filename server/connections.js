c20145d4eade4068bd034975495570410824f75e
AmySand <amy.sand@midlandu.edu>
2019-05-29 11:53:57 -0500

    connections.js updated

--- server/connections.js
+++ server/connections.js
@@ -1,28 +1,28 @@
-const mysql = require("mysql");
-
-let pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    database: "devSpeak",
-    password: "a1*L23s4j",
-    user: "amy",
-
-})
-
-module.exports.pool = pool;
-
-
-
 // const mysql = require("mysql");
-// require("./config/config")
+
 // let pool = mysql.createPool({
 //     connectionLimit: 10,
-//     host: process.env.hostname,
-//     database: process.env.database,
-//     password: process.env.password,
-//     // port: process.env.dbport,
-//     user: process.env.username,
+//     host: "localhost",
+//     database: "devSpeak",
+//     password: "a1*L23s4j",
+//     user: "amy",
 
 // })
 
 // module.exports.pool = pool;
+
+
+
+const mysql = require("mysql");
+require("./config/config")
+let pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.hostname,
+    database: process.env.database,
+    password: process.env.password,
+    // port: process.env.dbport,
+    user: process.env.username,
+
+})
+
+module.exports.pool = pool;
